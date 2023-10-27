bcc09ebc092607e8ab3df372d2b0528ca6ce0679
Ulrixon <ryan70235@gmail.com>
2023-10-27 16:45:11 -0700

    unit test for triangle complete

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,42 @@
 using shapes::Triangle;
 
 
-TEST(TriangleTests, testPerimeter) {
+TEST(TriangleTests, testPerimeter) { //goint to pass
     Triangle *aTriangle = new Triangle(3,3,3);
     EXPECT_EQ (aTriangle->getPerimeter(),9);
 }
+
+TEST(TriangleTests, testPerimeterWithSidesBeingDifferent) { //goint to fail
+    Triangle *aTriangle = new Triangle(3,3,2);
+    EXPECT_EQ (aTriangle->getPerimeter(),8);
+}
+
+TEST(TriangleTests, testAreaWith333) //goint to pass
+{
+    Triangle *aTriangle = new Triangle(3,3,3);
+    EXPECT_NEAR(aTriangle->getArea(), 3.897, 0.001);
+}
+
+TEST(TriangleTests, testClassWith333Negative) //goint to pass
+{
+    
+    EXPECT_DEATH(new Triangle(3,3,-3),"Does not satisfy triangle inequality");
+}
+
+TEST(TriangleTests, testEquilateral333) //goint to pass
+{
+    Triangle *aTriangle = new Triangle(3,3,3);
+    EXPECT_TRUE(aTriangle->isEquilateral());
+}
+
+TEST(TriangleTests, testIsosceles333) //goint to pass
+{
+    Triangle *aTriangle = new Triangle(3,3,3);
+    EXPECT_TRUE(aTriangle->isIsosceles());
+}
+
+int runTests(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
