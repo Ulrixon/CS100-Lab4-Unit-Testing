4f5a1f55cb816f7c3dd2838decc684447d1ce83b
Ulrixon <ryan70235@gmail.com>
2023-10-31 19:10:53 -0700

    complete hailstone test

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,24 @@
 #include "gtest/gtest.h"
 #include "../include/Hailstone.h"
+using sequence::satisfiesHailstone;
 
+
+TEST(HailstoneTests, testZero){
+    uint64_t a = 0;
+    EXPECT_FALSE(satisfiesHailstone(a));
+}
+
+TEST(HailstoneTests, testOne){
+    uint64_t a = 1;
+    EXPECT_TRUE(satisfiesHailstone(a));
+}
+
+TEST(HailstoneTests, testEven){
+    uint64_t a = 2;
+    EXPECT_TRUE(satisfiesHailstone(a));
+}
+
+TEST(HailstoneTests, testOdd){
+    uint64_t a = 3;
+    EXPECT_TRUE(satisfiesHailstone(a));
+}
